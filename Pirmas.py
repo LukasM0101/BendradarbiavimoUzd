e245bebd2cb1498a44774a811ae01a25531a0a98
Lukas M <lukasdai2@gmail.com>
2023-11-09 10:21:59 +0200

    Saule atsirado

--- Pirmas.py
+++ Pirmas.py
@@ -1,6 +1,15 @@
+import turtle
+
 print("Pirmas")
 print("123")
 print("Saulė geltona")
 
 x = "Saulė geltona"
 print(x.replace("ė", "e"))
+
+screen = turtle.Screen()
+screen.bgcolor("skyblue")
+sun = turtle.Turtle()
+sun.shape("circle")
+sun.color("yellow")
+screen.exitonclick()
